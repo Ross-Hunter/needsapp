4760bf16af0e8394078255e186ec44de9e840709
Ross Hunter <ross-hunter@ross-hunter.com>
2012-02-26 13:29:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Needsapp2::Application.config.secret_token = '7e1d1ca806f6fe944c0e1ca97f0d6e6232a748ee57b129dea096f261c75832bc90471457f32b6fb716828f3cb77c8c84c7c335c9c73d089530eb3be7c6bee03c'
