edf4f00f330898e74d53d6485c778251bb73df2f
Ross Hunter <ross-hunter@ross-hunter.com>
2012-04-01 22:47:13 -0400

    category debug

--- app/models/need.rb
+++ app/models/need.rb
@@ -2,7 +2,7 @@ class Need < ActiveRecord::Base
 
 	validates_presence_of :title, :date
 	has_many :slots
-	belongs_to :category, :foreign_key => :id
+	belongs_to :category, :foreign_key => :category_id
 
 	#scope :not_expired, where(:datetime1 => Date.today..(Date.today + 1.year))
 	scope :between, lambda{|start_date, end_date| where(date: start_date..end_date)}
