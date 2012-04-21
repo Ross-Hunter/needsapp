4ea86f9deeb7dc8fd36507127eb9f4ede4dd7ddb
Ross Hunter <ross-hunter@ross-hunter.com>
2012-04-20 21:39:30 -0400

    going to run in dev mode for now

--- app/controllers/admin/configurables_controller.rb
+++ app/controllers/admin/configurables_controller.rb
@@ -1,9 +1,14 @@
 class Admin::ConfigurablesController < ApplicationController
-
-	#include ConfigurableEngine::ConfigurablesController
+	include ConfigurableEngine::ConfigurablesController
 
 	before_filter :authenticate_admin
 
-
+  def authenticate_admin
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if !@current_user && (@current_user.id !=1)
+      redirect_to root_url, :notice => "You must be logged in and have permission to do that!"
+      return
+    end
+  end
 
 end
