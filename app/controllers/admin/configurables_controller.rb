27d9d0ca14aec0214755da28790cae8ce105aa7c
Ross Hunter <ross-hunter@ross-hunter.com>
2012-04-20 21:38:07 -0400

    prod debug

--- app/controllers/admin/configurables_controller.rb
+++ app/controllers/admin/configurables_controller.rb
@@ -1,15 +1,9 @@
 class Admin::ConfigurablesController < ApplicationController
-	Rails.application.eager_load!
+
 	#include ConfigurableEngine::ConfigurablesController
 
 	before_filter :authenticate_admin
 
-  def authenticate_admin
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    if !@current_user || @current_user.id !=1
-      redirect_to needs_url, :notice => "You must be logged in and have permission to do that!"
-      return
-    end
-  end
+
 
 end
